998a63c94462d1e7aac1da4393465b4b6ed84476
Tolga Pirim <tolgapirim25@gmail.com>
2023-04-12 14:56:59 +0300

    Implemented Stack for Kotlin

---
+++ Stack-Queque-Deque/stack/kotlin/StackImplementation.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val myStack = MyStack<Int>()
+
+    myStack.push(10)
+    myStack.push(20)
+    myStack.push(30)
+
+    println(myStack.isEmpty())
+
+    println(myStack.pop())
+    println(myStack.size())
+    println(myStack.pop())
+    println(myStack.pop())
+    println(myStack.isEmpty())
+}
+
+class MyStack<T> {
+    private val elements = mutableListOf<T>()
+
+    fun isEmpty() = elements.isEmpty()
+
+    fun size() = elements.size
+
+    fun push(element: T) {
+        elements.add(element)
+    }
+
+    fun pop(): T {
+        return elements.removeLast()
+    }
+}
