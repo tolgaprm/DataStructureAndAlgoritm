46e75e8ee544a15f7c92428b967c1f122fe002cc
Tolga Pirim <tolgapirim25@gmail.com>
2023-04-26 19:07:14 +0300

    Solved removeNthFromEnd in LinkedList problem

---
+++ LinkedList/ Remove_Nth_Node_From_End_of_List.kt
@@ -0,0 +1,27 @@
+class ListNode(val value: Int){
+    var next: ListNode? = null
+}
+
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    var previous: ListNode? = null
+    var slowPointer = head
+    var fastPointer = head
+
+    for (step in 0 until n) {
+        fastPointer = fastPointer?.next
+    }
+
+    while (fastPointer != null) {
+        previous = slowPointer
+        slowPointer = slowPointer?.next
+        fastPointer = fastPointer.next
+    }
+
+    if (previous == null) {
+        return head?.next
+    }
+
+    previous.next = slowPointer?.next
+
+    return head
+}
